26c73284192610d71531c6b2ed7bab26efcd2b69
Ivan Navarrete <crzivn@gmail.com>
2006-09-18 10:54:15 +0000

    Added simple command-line history through the Readline library.   Note that Readline is GPL.
    git-svn-id: file:///Users/ivan/Tmp/cleanup/2/projects/satori_rb/trunk@7 2d522cb3-f51c-0410-b290-8dde7a79f435

--- src/satori/ui/txt/basetxtui.rb
+++ src/satori/ui/txt/basetxtui.rb
@@ -1,4 +1,7 @@
 
+require 'readline'
+include Readline
+
 require 'satori/ui/txt/basecommandtable'
 require 'lib/ui/txt/txtui'
 
@@ -21,10 +24,15 @@ public
 	def idle
 		message("Satori version r0.1", "Welcome", "")
 
+		# load AVR module and a device (for easier testing, remove later)
+		exec("module avr")
+		@arch_ui.exec("device at90s8535") if @arch_ui
+
 		loop do
 			# get user input
-			print("#{Color.prompt}: #{Color.default}")
-			redo if (command_line = gets.strip) == ""
+			command_line = readline("#{Color.prompt}: #{Color.default}", true)
+			command_quit(nil) if command_line.nil?
+			redo if command_line.strip! == ""
 
 			# execute commands
 			executed = false
@@ -33,17 +41,19 @@ public
 			executed |= @comm_ui.exec(command_line) if @comm_ui
 			
 			# display error if no subsystem executed the command
-			if not executed then error("command not found [#{command_line}]")end
+			error("command not found [#{command_line}]") if not executed
 		end
 	end
 
 private
 	## Terminate program.
 	def command_quit(command)
+		puts if not command
+		message("shutting down")
 		exit 0
 	end
 
-	## Show available communication and architecture modules.
+	## Show available modules.
 	def command_show_module(command)
 		message("--[ Architecture modules ]--")
 		Dir.glob(File.join(@arch_dir,"*")) {|n| message("  #{n.slice(/\w+$/)}")}
@@ -57,12 +67,14 @@ private
 		module_name = command.arguments[0].strip
 		arch_dir = File.join(@arch_dir, module_name)
 
-		Dir.chdir(arch_dir) do
+		begin
 			$: << arch_dir
 			load "#{arch_dir}/#{module_name}.rb"
 			@arch_ui = Object.const_get("#{module_name.capitalize}TxtUi").new
 			message("architecture module loaded: #{module_name}")
-		end rescue error("module not found [#{module_name}]")
+		rescue LoadError, NameError
+			error("module not found [#{module_name}]")
+		end
 	end
 
 	## Clear screen.
